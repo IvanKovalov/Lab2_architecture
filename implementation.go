a3ccc15bda488a8909a255b40f66453dc9cb6802
IvanKovalov <kovalevvana8@gmail.com>
2022-05-08 20:52:22 +0300

    1 task

--- implementation.go
+++ implementation.go
@@ -1,9 +1,41 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // TODO: document this function.
 // PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+var a string = "4 2 - 3 * 5 +"
+
+func PostfixToInfix(s string) (string, error) {
+
+	var strerr string
+	str := strings.Split(s, " ")
+	lenght := len(str)
+	if lenght == 0 {
+		strerr = "Пустая строчка"
+	}
+
+	if strings.Contains(s, `^[a-zA-Z]+$`) {
+		strerr = "Недопустимые символы"
+	}
+
+	for i := 0; i < lenght; i++ {
+		if str[i] == "+" || str[i] == "-" || str[i] == "*" || str[i] == "/" || str[i] == "^" {
+			prev := str[i-1]
+			symbol := str[i]
+			str[i-1] = symbol
+			str[i] = prev
+		}
+
+	}
+	sentence := strings.Join(str, "")
+	//fmt.Println(sentence)
+	return sentence, fmt.Errorf(strerr)
+}
+
+func main() {
+	//PostfixToInfix(a)
 }
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,43 @@
 package lab2
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "gopkg.in/check.v1"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var suit = Suite(&MySuite{})
+
+func (s *MySuite) TestPostfixToInfix(c *C) {
+	res, _ := PostfixToInfix("4 2 - 3 * 5 + ")
+	c.Check(res, Equals, "4-2*3+5")
+}
+
+func (s *MySuite) TestBigPostfixToInfix(c *C) {
+	res, _ := PostfixToInfix("4 2 - 3 * 5 + 6 + 2 * 1 /")
+	c.Check(res, Equals, "4-2*3+5+6*2/1")
+}
+
+func (s *MySuite) TestErrorPostfixToInfix(c *C) {
+	_, err := PostfixToInfix("")
+	c.Check(err, Equals, errors.New("Пустая строчка"))
+}
+
+func (s *MySuite) ExamplePostfixToInfix(c *C) {
+	res, _ := PostfixToInfix("42+")
+	c.Check(res, Equals, "4+2")
+	fmt.Println(res)
 }
 
 func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+	res, _ := PostfixToInfix("2 2 +")
 	fmt.Println(res)
-
-	// Output:
-	// 2 2 +
+	//Output:
+	// 2+2
 }
