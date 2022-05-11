6f5279d4b156e1456469342a2a145f6b22b84e36
IvanKovalov <kovalevvana8@gmail.com>
2022-05-11 15:51:31 +0300

    2 task fix_v2

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,8 +4,9 @@ import (
 	//"errors"
 	"flag"
 	"fmt"
-
+	"io"
 	"os"
+	"strings"
 
 	lab2 "github.com/devteam/lab-2"
 )
@@ -16,6 +17,56 @@ var (
 	outputResult    = flag.Bool("o", true, "Write result to file")
 )
 
+type Reader interface {
+	Read(str string)
+}
+
+type Writer interface {
+	Write(str string, outFile string)
+}
+
+type Readall struct{}
+
+type WriteAll struct{}
+
+func (rd Readall) Read(inputStr string) string {
+	if strings.Contains(inputStr, ".txt") {
+		file, err := os.Open(inputStr)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		data := make([]byte, 64)
+		str := make([]string, 0)
+
+		for {
+			n, err := file.Read(data)
+			if err == io.EOF { // если конец файла
+				break // выходим из цикла
+			}
+			str = append(str, string(data[:n]))
+		}
+		result := strings.Join(str, " ")
+		return result
+	}
+	return inputStr
+}
+
+func (wr WriteAll) Write(str string, outFile string) {
+	f, err := os.OpenFile(outFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.Write([]byte(str))
+}
+
+var writer = WriteAll{}
+var reader = Readall{}
+
 func main() {
 	flag.Parse()
 	in := os.Args[2]
@@ -23,40 +74,40 @@ func main() {
 	if *inputExpression && *outputResult {
 
 		handler := &lab2.ComputeHandler{
+			Wr:     writer,
+			Rd:     reader,
 			Input:  in,
-			Output: os.Args[4],
+			Output: "default",
 		}
 		handler.Compute()
 		fmt.Println(inputExpression)
 	} else if *inputExpression && !*outputResult {
 		handler := &lab2.ComputeHandler{
+			Wr:     writer,
+			Rd:     reader,
 			Input:  os.Args[2],
-			Output: "default",
+			Output: os.Args[4],
 		}
 		handler.Compute()
 	} else if *inputFile && !*outputResult {
 		handler := &lab2.ComputeHandler{
+			Wr:     writer,
+			Rd:     reader,
 			Input:  os.Args[2],
-			Output: os.Args[4],
+			Output: "default",
 		}
 		fmt.Println(handler.Compute())
 	} else if *inputFile && *outputResult {
 		//out := os.Args[4]
 		handler := &lab2.ComputeHandler{
+			Wr:     writer,
+			Rd:     reader,
 			Input:  os.Args[2],
-			Output: "default",
+			Output: os.Args[4],
 		}
 		handler.Compute()
 	} else if !*inputExpression {
 		//err := errors.New("Input expretion")
 	}
-
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
 
 }
--- handler.go
+++ handler.go
@@ -2,80 +2,35 @@ package lab2
 
 import (
 	"fmt"
-	"io"
-	"os"
-	"strings"
 )
 
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type Reader interface {
-	Read(str string)
+	Read(inputStr string) string
 }
 
 type Writer interface {
-	Write(str string)
+	Write(str string, outFile string)
 }
 
 type ComputeHandler struct {
 	// TODO: Add necessary fields.
+	Rd     Reader
+	Wr     Writer
 	Input  string
 	Output string
-}
-
-func (ch ComputeHandler) Read() string {
-	if strings.Contains(ch.Input, ".txt") {
-		file, err := os.Open(ch.Input)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer file.Close()
-
-		data := make([]byte, 64)
-		str := make([]string, 0)
-
-		for {
-			n, err := file.Read(data)
-			if err == io.EOF { // если конец файла
-				break // выходим из цикла
-			}
-			str = append(str, string(data[:n]))
-			//fmt.Print(string(data[:n]))
-		}
-		result := strings.Join(str, " ")
-		return result
-	}
-	return ch.Input
-}
-
-func (ch ComputeHandler) Write(str string) {
-	f, err := os.OpenFile(ch.Output, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	f.Write([]byte(str))
 }
 
 func (ch *ComputeHandler) Compute() error {
 	// TODO: Implement.
 
-	ex := ch.Read()
+	ex := ch.Rd.Read(ch.Input)
 	res, _ := PostfixToInfix(ex)
 	if ch.Output == "default" {
 		fmt.Println(res)
 	} else {
-		ch.Write(res)
+		ch.Wr.Write(res, ch.Output)
 	}
 	return nil
 }
-
-//var st1 = ComputeHandler{"4 3 -", "default"}
-
-//var st2 = st1.Compute()
-
-//func main() {
-//fmt.Println(st2)
-//}
